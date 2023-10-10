23566c895348d0f4ad3fda485e15507402331e3b
takemasa <s215016@matsu.shimane-u.ac.jp>
2023-10-10 07:12:22 +0000

    usersのデータベースを作成

--- SNS/db/schema.rb
+++ SNS/db/schema.rb
@@ -10,17 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_075441) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_065435) do
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "name"
+    t.text "email"
+    t.string "password"
+    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
