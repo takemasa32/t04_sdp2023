60481ebbc4bb3009bbe7c74f495ee42e81c5e4aa
akase taiyou <s215003@matsu.shimane-u.ac.jp>
2023-10-10 09:47:56 +0000

    最後のやつ実行しました

--- SNS/db/migrate/20231010083737_create_posts_tags.rb
+++ SNS/db/migrate/20231010083737_create_posts_tags.rb
@@ -3,7 +3,7 @@ class CreatePpostsTags < ActiveRecord::Migration[7.0]
     create_table :posts_tags do |t|
       t.integer  :post_id, foreign_key: true
       t.integer  :tag_id, foreign_key: true
-      t.timestamps
+      t.timestamp
     end
   end
 end
