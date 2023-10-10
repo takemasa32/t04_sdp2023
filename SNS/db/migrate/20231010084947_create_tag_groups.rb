01056f8850048eaa49641e9fd48a512dababfa28
matsuishi <s215042@matsu.shimane-u.ac.jp>
2023-10-10 09:26:25 +0000

    tag_groupsデータベースの作成2

--- SNS/db/migrate/20231010084947_create_tag_groups.rb
+++ SNS/db/migrate/20231010084947_create_tag_groups.rb
@@ -1,7 +1,7 @@
 class CreateTagGroups < ActiveRecord::Migration[7.0]
   def change
-    create_table :tag_groups do |t|
-      t.string :name
+    create_table    :tag_groups do |t|
+      t.string      :name 
 
       t.timestamps
     end
