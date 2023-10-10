5940470456a638223e9ffcbe6c9ed943ed23960a
Takahashi Haruto <s225032@matsu.shimane-u.ac.jp>
2023-10-10 09:37:15 +0000

    4-likes機能

--- SNS/db/schema.rb
+++ SNS/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_072028) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_084314) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
     t.text "body"
