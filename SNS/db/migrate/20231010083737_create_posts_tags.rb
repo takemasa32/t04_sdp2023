ca4cebd499898f34cf68aa69f6e39311ee21183b
s225018 kamihashi <s225018@matsu.shimane-u.ac.jp>
2023-10-17 08:29:44 +0000

    エラーの修正

--- SNS/db/migrate/20231010083737_create_posts_tags.rb
+++ SNS/db/migrate/20231010083737_create_posts_tags.rb
@@ -1,9 +1,9 @@
-class CreatePpostsTags < ActiveRecord::Migration[7.0]
+class CreatePostsTags < ActiveRecord::Migration[7.0]
   def change
     create_table :posts_tags do |t|
       t.integer  :post_id, foreign_key: true
       t.integer  :tag_id, foreign_key: true
-      t.timestamp
+      t.timestamps
     end
   end
 end
