3b73334a6052537237d84956482c08582e408fa3
akase taiyou <s215003@matsu.shimane-u.ac.jp>
2023-10-17 07:06:44 +0000

    CONFLICTEDを解消

--- SNS/config/environments/development.rb
+++ SNS/config/environments/development.rb
@@ -68,6 +68,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 config.hosts << "a9b9dd00398b4d3483abba1a95760ea2.vfs.cloud9.us-east-1.amazonaws.com"
-
-config.hosts << "032ec37df18f4876a52ba96a1f5d453e.vfs.cloud9.us-east-1.amazonaws.com"
 end
