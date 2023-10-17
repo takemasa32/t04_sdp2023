cecd39223c2aabfc963715edded9100f4d4ad2c4
s225018 kamihashi <s225018@matsu.shimane-u.ac.jp>
2023-10-17 01:49:11 +0000

    ファイルのミスを修正して、migrateを行いました

--- SNS/db/migrate/20231010082427_create_posts.rb
+++ SNS/db/migrate/20231010082427_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.integer :user_id, foreign_key: true
       t.text :title
       t.text :body
-      t.image :user_id
+      t.text :image
       t.text :role
 
 
