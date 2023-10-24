d7557fc184c93abf64e5f599e9a0c60dcca8cc1a
takemasa <s215016@matsu.shimane-u.ac.jp>
2023-10-24 07:42:05 +0000

    基本機能作成完了

--- SNS/db/schema.rb
+++ SNS/db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_062357) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "email"
-    t.string "password"
+    t.string "password_digest"
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
