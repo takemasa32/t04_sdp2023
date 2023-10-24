019e788b1c12e3ee5521f766496704737973d457
Taishin Aoki <s215001@matsu.shimane-u.ac.jp>
2023-10-24 09:30:25 +0000

    resourcesメソッドのusersを追加

--- SNS/config/routes.rb
+++ SNS/config/routes.rb
@@ -17,4 +17,10 @@ Rails.application.routes.draw do
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
-end
+
+
+  resources :users
+  
+   puts 'Hello'
+ 
+ end
