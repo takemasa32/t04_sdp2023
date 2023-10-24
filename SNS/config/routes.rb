c220e27b9437c098b7d2cd720a01e5410f79e465
matsuishi <s215042@matsu.shimane-u.ac.jp>
2023-10-24 09:38:52 +0000

    画像を保存できるように変更

--- SNS/config/routes.rb
+++ SNS/config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: %i[new create show destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
     # 以下を追記
-    get '/signup', to: 'users#new'
-    post '/signup', to: 'users#create'
+    
+    #get '/signup', to: 'users#new'
+    #post '/signup', to: 'users#create'
+    
     # get '/users/:id', to: 'users#show', as: 'profile' # 予備
     # delete '/users/:id', to: 'users#destroy', as: 'unsubscribe' # 予備
 
-    get '/profile', to: 'users#show'
-    delete '/unsubscribe', to: 'users#destroy'
+    #get '/profile', to: 'users#show'
+    #delete '/unsubscribe', to: 'users#destroy'
 
-    get '/login', to: 'sessions#new'
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
+    #get '/login', to: 'sessions#new'
+    #post '/login', to: 'sessions#create'
+    #delete '/logout', to: 'sessions#destroy'
 end
