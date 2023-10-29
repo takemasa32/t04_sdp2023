4acd8a6b41f3bb42a6964927259e1d6e82b1aacc
takemasa <s215016@matsu.shimane-u.ac.jp>
2023-10-29 06:10:42 +0000

    PostsのViewの修正

--- SNS/config/routes.rb
+++ SNS/config/routes.rb
@@ -17,4 +17,7 @@ Rails.application.routes.draw do
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
+
+  # 画像ファイルにアクセスするためのルートを追加
+    get '/images/:filename', to: 'images#show', as: 'image'
 end
