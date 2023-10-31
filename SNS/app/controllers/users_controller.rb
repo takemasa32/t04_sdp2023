06a6c192fed4f9fb9cde5fb229b19e239f71a903
takemasa <s215016@matsu.shimane-u.ac.jp>
2023-10-31 06:16:21 +0000

    更新(ログイン関係にバグあり)

--- SNS/app/controllers/users_controller.rb
+++ SNS/app/controllers/users_controller.rb
@@ -1,34 +1,31 @@
 class UsersController < ApplicationController
     before_action :require_login, only: [:show, :destroy]
-  def new
-    @user = User.new
-  end
+ # 以下を追記
+ def new
+  @user = User.new
+end
 
-  def create
-    # 以下を修正：コントローラーのリファクタリング②
-    user = User.new(user_params)
-    if user.save
-    # 以下を追記：コントローラーのリファクタリング③
-      log_in(user)
-      redirect_to profile_path(user)
-    else
-      render 'new'
-    end
+def create
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to profile_path(@user)
+  else
+    render 'new'
   end
+end
 
-  def show
-    @user = User.find(params[:id])
+def show
+  @user = User.find(params[:id])
+end
+
+def destroy
+  user = User.find(params[:id])
+  user.destroy
+  redirect_to signup_path
+end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
-  def destroy
-    user = User.find(params[:id])
-    # 以下を修正：コントローラーのリファクタリング④
-    current_user.destroy
-    redirect_to signup_path
-  end
-
-  private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
 end
