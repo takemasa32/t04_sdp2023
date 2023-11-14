7503a3c78eb0a83a73480da6be2fa16f02af8638
matsuishi <s215042@matsu.shimane-u.ac.jp>
2023-11-14 05:18:21 +0000

    画像のアップロードができるようになった

--- SNS/app/uploaders/board_image_uploader.rb
+++ SNS/app/uploaders/board_image_uploader.rb
@@ -1,4 +1,4 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class BoardImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -9,17 +9,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  def default_url
-    'sample.jpg'
-  end
-  
-  def extension_allowlist
-    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
