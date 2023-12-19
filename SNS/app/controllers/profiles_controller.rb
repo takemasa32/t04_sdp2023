39067c13931dcc4c232097472ee4cfd95b61ea41
takemasa <s215016@matsu.shimane-u.ac.jp>
2023-12-19 06:51:54 +0000

    アップデート

--- SNS/app/controllers/profiles_controller.rb
+++ SNS/app/controllers/profiles_controller.rb
@@ -5,6 +5,7 @@ class ProfilesController < ApplicationController
   # GET /profiles or /profiles.json
   def index
     @profiles = Profile.all
+    @posts = Post.all
   end
 
   # GET /profiles/1 or /profiles/1.json
